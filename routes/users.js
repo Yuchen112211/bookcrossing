87570d70c6842fe661e4a38eddf832ffc32382be
zacw7 <zac.wen7@gmail.com>
2021-04-01 17:00:38 -0700

    Implement API for retrieving user info

--- routes/users.js
+++ routes/users.js
@@ -30,6 +30,50 @@ router.post("/getUser", function (req, res, next) {
   });
 });
 
+router.get("/info/:username", function (req, res, next) {
+  console.log('Cookies: ', req.cookies);
+  console.log('req.params: ', req.params);
+
+  uid = req.cookies.uid;
+  username = req.params.username;
+  
+  db.selectOne("users", {"username": username}, function (user) {
+    if (!user) {
+      return res.status(404).json({msg: "user not found"});
+    }
+    user.sent = [];
+    user.received = [];
+    user.traveling = [];
+    userId = user._id.toString();
+    db.select("crossings", { $or: [ { fromId: userId }, { toId: userId} ] }, function (crossings) {
+      for (i in crossings) {
+        if (crossings[i].fromId === userId) {
+          if (crossings[i].receivedAt) {
+            user.sent.push(crossings[i]);
+          } else {
+            user.traveling.push(crossings[i]);
+          }
+        } else if (crossings[i].toId === userId && crossings[i].receivedAt) {
+          user.received.push(crossings[i]);
+        }
+        delete crossings[i]._id;
+        delete crossings[i].fromId;
+        delete crossings[i].toId;
+      }
+      if (uid === userId) {
+      
+      } else {
+        delete user.mailingAddress
+      }
+      delete user.password;
+      delete user.datetime;
+      delete user._id;
+      console.log(user);
+      return res.status(200).json({ msg: "success", data: user});
+    });
+  });
+});
+
 router.get("/getRandom", function (req, res, next) {
   db.getRandom("users", {}, function (data) {
     console.log(
